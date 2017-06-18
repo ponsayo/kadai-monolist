d731c2b46bde36fb691a53d28e1bd7b000b0fe87
ponsayo <tigerbook3420@gmail.com>
2017-06-18 11:25:04 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     end
   end
   
+  get 'rankings/want', to: 'rankings#want'
+  
   resources :items, only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
 end
